23229e1c9e0b9d8b66dd750c3f2ccee7fa624678
Travis Kinney <travis@traviskinney.co>
2025-06-18 21:32:14 -0700

    Add settings from user.js and userChrome.css

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -14,6 +14,22 @@ in {
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+
+      profiles.default = {
+        settings = {
+          "browser.privateWindowSeparation.enabled" = false;
+          "browser.tabs.loadBookmarksInBackground" = true;
+          "extensions.autoDisableScopes" = 0;
+          "extensions.pocket.enabled" = false;
+          "full-screen-api.transition-duration.enter" = "0 0";
+          "full-screen-api.transition-duration.leave" = "0 0";
+          "full-screen-api.warning.timeout" = 0;
+        };
+
+        userChrome = ''
+          @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
+        '';
+      };
     };
   };
 }
