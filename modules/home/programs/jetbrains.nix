d681e6434fca988303d64cd4d670761576117992
Travis Kinney <travis@traviskinney.co>
2025-07-22 21:59:30 -0700

    Add Wayland support for JetBrains IDEs

--- modules/home/programs/jetbrains.nix
+++ modules/home/programs/jetbrains.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: let
-  inherit (lib) concatLists mkEnableOption optional;
+  inherit (lib) any attrValues concatLists mkEnableOption mkIf optional;
   inherit (config.lib.file) mkOutOfStoreSymlink;
   cfg = config.settings.programs.jetbrains;
 in {
@@ -20,6 +20,11 @@ in {
   # https://wiki.nixos.org/wiki/Jetbrains_Tools
   # https://github.com/NixOS/nixpkgs/issues/240444
   config = let
+    withOpts = pkg:
+      pkg.override {
+        vmopts = "-Dawt.toolkit.name=WLToolkit";
+      };
+
     withPlugins = pkg:
       with pkgs.jetbrains; (
         plugins.addPlugins pkg [
@@ -32,14 +37,14 @@ in {
   in {
     home.packages = with pkgs;
       concatLists [
-        (optional cfg.toolbox.enable jetbrains-toolbox)
-        (optional cfg.datagrip.enable (withPlugins jetbrains.datagrip))
-        (optional cfg.goland.enable (withPlugins jetbrains.goland))
-        (optional cfg.pycharm.enable (withPlugins jetbrains.pycharm-professional))
-        (optional cfg.rider.enable (withPlugins jetbrains.rider))
+        (optional cfg.toolbox.enable (withOpts jetbrains-toolbox))
+        (optional cfg.datagrip.enable (withPlugins (withOpts jetbrains.datagrip)))
+        (optional cfg.goland.enable (withPlugins (withOpts jetbrains.goland)))
+        (optional cfg.pycharm.enable (withPlugins (withOpts jetbrains.pycharm-professional)))
+        (optional cfg.rider.enable (withPlugins (withOpts jetbrains.rider)))
       ];
 
-    home.file.".ideavimrc" = {
+    home.file.".ideavimrc" = mkIf (any (x: x.enable) (attrValues cfg)) {
       source = mkOutOfStoreSymlink "/etc/nixos/files/config/jetbrains/.ideavimrc";
     };
   };
