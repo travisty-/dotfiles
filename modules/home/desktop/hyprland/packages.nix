c6314a9932c4c0fb1ad16640bb2884a3d3fda689
Travis Kinney <travis@traviskinney.co>
2025-08-06 17:34:55 -0700

    Add Hyprpicker to Hyprland packages

---
+++ modules/home/desktop/hyprland/packages.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf;
+  cfg = config.settings.desktop.hyprland;
+in {
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      hyprpicker
+    ];
+  };
+}
