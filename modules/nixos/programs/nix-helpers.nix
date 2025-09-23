6e5ee0bccbd188e1b99d65dcf2aaf28e9405203c
Travis Kinney <travis@traviskinney.co>
2025-09-23 00:51:57 -0700

    Optimise Nix store after garbage collection

--- modules/nixos/programs/nix-helpers.nix
+++ modules/nixos/programs/nix-helpers.nix
@@ -11,11 +11,12 @@ in {
     enable = mkEnableOption "Nix helpers";
   };
 
+  # https://nixos.wiki/wiki/Storage_optimization
   config = mkIf cfg.enable {
     programs.nh = {
       enable = true;
       clean.enable = true;
-      clean.extraArgs = "--keep-since 7d --keep 5";
+      clean.extraArgs = "--keep-since 7d --keep 5 --optimise";
       flake = "/etc/nixos";
     };
 
