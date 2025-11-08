af5daf45044e39ab0c58845f1db1d13d7a272754
Travis Kinney <travis@traviskinney.co>
2025-11-08 13:33:07 -0800

    Add default settings for Vicinae

--- modules/home/services/vicinae.nix
+++ modules/home/services/vicinae.nix
@@ -21,6 +21,21 @@ in {
     services.vicinae = {
       enable = true;
       autoStart = true;
+      settings = {
+        faviconService = "twenty";
+        font.size = 10.5;
+        popToRootOnClose = true;
+        rootSearch.searchFiles = false;
+        theme.name = "vicinae-dark";
+        window = {
+          csd = true;
+          opacity = 0.95;
+          rounding = 10;
+        };
+      };
     };
+
+    # Workaround to avoid creating a new backup every switch.
+    xdg.configFile."vicinae/vicinae.json".force = true;
   };
 }
