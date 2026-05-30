6aa1aca458473b4cd02f566f5e21168c13e9ec07
Travis Kinney <travis@traviskinney.co>
2026-05-29 22:21:56 -0700

    Update flake.lock

--- modules/features/shared/workarounds.nix
+++ modules/features/shared/workarounds.nix
@@ -1,16 +1,7 @@
 # Temporary workarounds for upstream Nixpkgs issues. Each entry should link
 # its tracking issue and be removed when the issue is closed upstream.
-let
-  # https://github.com/NixOS/nixpkgs/issues/523257
-  _1password-gui = _: prev: {
-    _1password-gui = prev._1password-gui.overrideAttrs (oldAttrs: {
-      src = oldAttrs.src.overrideAttrs {
-        outputHash = "sha256-JwiMi2iozP6jWSIUtgXla86aSAhuUob7snqtUbeXPpI=";
-      };
-    });
-  };
-in {
-  flake.modules.homeManager.base.nixpkgs.overlays = [_1password-gui];
+{
+  flake.modules.homeManager.base.nixpkgs.overlays = [];
 
   flake.modules.nixos.base.nixpkgs.overlays = [
     # https://github.com/NixOS/nixpkgs/issues/514113
@@ -19,7 +10,5 @@ in {
         doCheck = !prev.stdenv.hostPlatform.isi686;
       };
     })
-
-    _1password-gui
   ];
 }
