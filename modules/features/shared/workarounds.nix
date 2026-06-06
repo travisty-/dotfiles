73590a1936e078f049b94e0c083db9cc2e81cb45
Travis Kinney <travis@traviskinney.co>
2026-06-05 20:35:30 -0700

    Update flake.lock

--- modules/features/shared/workarounds.nix
+++ modules/features/shared/workarounds.nix
@@ -3,12 +3,5 @@
 {
   flake.modules.homeManager.base.nixpkgs.overlays = [];
 
-  flake.modules.nixos.base.nixpkgs.overlays = [
-    # https://github.com/NixOS/nixpkgs/issues/514113
-    (_: prev: {
-      openldap = prev.openldap.overrideAttrs {
-        doCheck = !prev.stdenv.hostPlatform.isi686;
-      };
-    })
-  ];
+  flake.modules.nixos.base.nixpkgs.overlays = [];
 }
