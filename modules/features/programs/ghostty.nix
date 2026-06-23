d6f17472e6c261735bc755617bd2e3e2f9801182
Travis Kinney <travis@traviskinney.co>
2026-06-23 02:06:53 -0700

    Disable most ligatures (font-feature)

--- modules/features/programs/ghostty.nix
+++ modules/features/programs/ghostty.nix
@@ -9,7 +9,7 @@
         background-blur-radius = 20;
         background-opacity = 0.95;
         font-family = "GeistMono NF";
-        font-feature = "-calt"; # -liga, -dlig
+        font-feature = "-calt, -liga, -dlig";
         font-size = 14;
         keybind = [
           "shift+enter=text:\\n"
