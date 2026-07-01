0c34e90140d447e592775ba54c30f8b1ad9448e4
Travis Kinney <travis@traviskinney.co>
2026-07-01 01:44:46 -0700

    Use XDG base directories (dev tools)

--- modules/features/shared/xdg.nix
+++ modules/features/shared/xdg.nix
@@ -1,5 +1,13 @@
 {
-  flake.modules.homeManager.base = {
+  flake.modules.homeManager.base = {config, ...}: {
     xdg.enable = true;
+
+    # XDG environment variables for development tools.
+    home.sessionVariables = {
+      CARGO_HOME = "${config.xdg.dataHome}/cargo";
+      GOMODCACHE = "${config.xdg.cacheHome}/go/mod";
+      GOPATH = "${config.xdg.dataHome}/go";
+      NPM_CONFIG_CACHE = "${config.xdg.cacheHome}/npm";
+    };
   };
 }
