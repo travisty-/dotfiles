4d171e593aff4c251ab49ebd0319f6d16e8d835d
Travis Kinney <travis@traviskinney.co>
2026-07-01 01:44:46 -0700

    Use XDG base directories (docker)

--- modules/features/programs/docker.nix
+++ modules/features/programs/docker.nix
@@ -12,5 +12,8 @@
       };
       storageDriver = lib.mkIf (config.fileSystems."/".fsType == "btrfs") "btrfs";
     };
+
+    # Relocate ~/.docker/config into $XDG_CONFIG_HOME.
+    environment.sessionVariables.DOCKER_CONFIG = "$HOME/.config/docker";
   };
 }
