577b91db6e5283f55e55df436811772060a01f08
Travis Kinney <travis@traviskinney.co>
2026-08-01 15:31:32 -0700

    Use pipe operators instead of lib.pipe

--- modules/features/desktops/niri/_scripts.nix
+++ modules/features/desktops/niri/_scripts.nix
@@ -1,13 +1,12 @@
 {pkgs}: let
   inherit (builtins) readDir readFile;
-  inherit (pkgs.lib) filterAttrs mapAttrsToList pipe;
+  inherit (pkgs.lib) filterAttrs mapAttrsToList;
   inherit (pkgs) symlinkJoin writeShellScriptBin;
 in
   symlinkJoin {
     name = "niri-scripts";
-    paths = pipe (readDir ./scripts) [
-      (filterAttrs (_: type: type == "regular"))
-      (mapAttrsToList (file: _:
-          writeShellScriptBin file (readFile (./scripts + "/${file}"))))
-    ];
+    paths =
+      readDir ./scripts
+      |> filterAttrs (_: type: type == "regular")
+      |> mapAttrsToList (file: _: writeShellScriptBin file (readFile (./scripts + "/${file}")));
   }
