61842a6cde4f6ebe0da4a053b1526d02243b7496
Travis Kinney <travis@traviskinney.co>
2026-09-05 14:04:05 -0700

    Disable tray icon and encrypt sensitive data (vicinae)

--- modules/features/programs/vicinae.nix
+++ modules/features/programs/vicinae.nix
@@ -18,7 +18,9 @@
         close_on_focus_loss = true;
         pop_to_root_on_close = true;
         search_files_in_root = false;
+        encrypt_sensitive_data = true;
         favicon_service = "twenty";
+        tray.enabled = false;
         font.normal.size = 10.5;
         # font.normal.normal = "Maple Nerd Font";
         theme.light.name = "vicinae-light";
