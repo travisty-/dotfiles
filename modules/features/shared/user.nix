6e00db60933e0cab953bb354ef55ba2050048de7
Travis Kinney <travis@traviskinney.co>
2026-09-11 19:24:07 -0700

    Remove empty per-user package list (user)

--- modules/features/shared/user.nix
+++ modules/features/shared/user.nix
@@ -1,9 +1,5 @@
 {
-  flake.modules.nixos.base = {
-    config,
-    pkgs,
-    ...
-  }: let
+  flake.modules.nixos.base = {config, ...}: let
     inherit (config.meta) user;
   in {
     users.defaultUserShell = user.shell;
@@ -11,9 +7,6 @@
       isNormalUser = true;
       inherit (user) description;
       extraGroups = ["networkmanager" "wheel"];
-      packages = with pkgs; [
-        # thunderbird
-      ];
     };
   };
 }
